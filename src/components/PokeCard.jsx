cd8d243ceef7aa812c548185be22344280b8495d
Alberto <a.g.arauzo13@gmail.com>
2024-04-29 10:21:46 +0200

    boton para refrescar operativo, programa estructurado, falta poner el estilo

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,44 +1,70 @@
 import { useState,useEffect } from "react";
+import { generarNumeroAleatorio } from "../services/num_aleatorio";
 
 export function PokeCard() {
 
     const [pokemon, setPokemon] = useState(null)
-    const num_pokemon = Math.floor(Math.random() * 151) +1
-    console.log(num_pokemon)
+    const [num, setNum] = useState(Math.floor(Math.random() * 151) +1)
+    console.log(num)
 
     useEffect(() => {
         // recuperamos la api
-        fetch(`https://pokeapi.co/api/v2/pokemon/${num_pokemon}/`)
+        fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`)
             .then(res => res.json())
             .then(response => {
                 console.log(response)
                 setPokemon(response)
             })
-    }, [])
+    }, [num])
 
-    console.log(pokemon?.sprites.other.showdown['back_default'])
+    const handleClick = () => {
+        generarNumeroAleatorio().then(newNum => setNum(newNum))
+    }
+
+    console.log(pokemon?.base_experience)
     console.log(pokemon?.stats[0].base_stat)
 
     return (
         //! tenemos que verificar que pokemon existe porque si no da error
         //! NO PUEDE SER EN EL DIV PRINCIPAL
         <>
-            {pokemon && <div>
-                <img src={pokemon?.sprites.other.showdown['back_default']} alt="culito-culito" />
-                <img src={pokemon?.sprites.other.showdown['front_default']} alt="culito-culito" />
-                <h2>{pokemon?.name}</h2>
-                <ul>
-                    <li>{pokemon?.stats[0].stat.name}: {pokemon?.stats[0].base_stat}</li>
-                    <li>{pokemon?.stats[1].stat.name}: {pokemon?.stats[1].base_stat}</li>
-                    <li>{pokemon?.stats[2].stat.name}: {pokemon?.stats[2].base_stat}</li>
-                    <li>{pokemon?.stats[3].stat.name}: {pokemon?.stats[3].base_stat}</li>
-                    <li>{pokemon?.stats[4].stat.name}: {pokemon?.stats[4].base_stat}</li>
-                    <li>{pokemon?.stats[5].stat.name}: {pokemon?.stats[5].base_stat}</li>
-                </ul>
+            {pokemon && <article>
+                {/* aqui iria la imagen con el fondo */}
+                <div className="infoPokemon">
+                    <img src={pokemon?.sprites.other.showdown['front_default']} alt="gifPokemon" />
+                    <h1 className="namePokemon">
+                        {pokemon?.name}  
+                        <span> {pokemon?.stats[0].base_stat}{pokemon?.stats[0].stat.name}</span>
+                    </h1>
+                    <p className="expPokemon">{pokemon?.base_experience} exp</p>
+                </div>
+                <div className="statsPokemon">
+                    <div className="statsPokemon">
+                        <h3>{pokemon?.stats[1].base_stat}</h3>
+                        <p>{pokemon?.stats[1].stat.name}</p>
+                    </div>
+                    <div className="statsPokemon">
+                        <h3>{pokemon?.stats[2].base_stat}</h3>
+                        <p>{pokemon?.stats[2].stat.name}</p>
+                    </div>
+                    <div className="statsPokemon">
+                        <h3>{pokemon?.stats[3].base_stat}</h3>
+                        <p>{pokemon?.stats[3].stat.name}</p>
+                    </div>
+                    <div className="statsPokemon">
+                        <h3>{pokemon?.stats[4].base_stat}</h3>
+                        <p>{pokemon?.stats[4].stat.name}</p>
+                    </div>
+                    <div className="statsPokemon">
+                        <h3>{pokemon?.stats[5].base_stat}</h3>
+                        <p>{pokemon?.stats[5].stat.name}</p>
+                    </div>
+                </div>
+                <button onClick={handleClick}>
+                    Nuevo Pokemon
+                </button>
 
-                <button>Nuevo Pokemon</button>
-
-            </div>}
+            </article>}
         </>
     )
 }
