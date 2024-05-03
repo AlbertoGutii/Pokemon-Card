0a1ba6d608b4f3e55e2006e53b1a2554f902e153
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2024-05-03 10:22:46 +0200

    sugerencias

--- src/services/num_aleatorio.js
+++ src/services/num_aleatorio.js
@@ -1,3 +1,7 @@
 export const generarNumeroAleatorio = async () => {
     return Math.floor(Math.random() * (152 - 1)) + 1;
-}
+};
+/* buena idea sacar la funcion del componente pero
+1. esta funcion solo alcula un numero, las has puesto async pero no hace nada asyncrono.
+2. la funcion no es un servicio, si no mas bien una funcion de utilidad. Estas funciones
+se suene poner en una carpeta lib/ o utils/ en un archivo helpers.ts o utils.ts */
